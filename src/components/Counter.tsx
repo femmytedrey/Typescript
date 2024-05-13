1ec1c222b9ffec5417ce969c1c25d895fd79abf0
femmytedrey <femmytedrey@gmail.com>
2024-05-13 13:58:58 +0100

    Reducer with typescript

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,32 +1,44 @@
-import { useReducer } from "react"
+import { useReducer } from "react";
 
-const initialState = { count: 0 }
+const initialState = { count: 0 };
 
 type CounterState = {
-    count: number
-}
+  count: number;
+};
 
-type actionState = {
-    payload: number
-    type: string
-}
-function reducer(state: CounterState, action: actionState){
-    switch(action.type){
-        case 'increment':
-            return { count: state.count + action.payload }
-        case 'decrement':
-            return { count: state.count - action.payload }
-        default:
-            return state
-    }
+type UpdateAction = {
+  type: "increment" | "decrement";
+  payload: number;
+};
+
+type ResetAction = {
+  type: "reset";
+};
+type actionState = UpdateAction | ResetAction;
+function reducer(state: CounterState, action: actionState) {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + action.payload };
+    case "decrement":
+      return { count: state.count - action.payload };
+    case "reset":
+      return initialState;
+    default:
+      return state;
+  }
 }
 export const Counter = () => {
-    const [state, dispatch] = useReducer(reducer, initialState);
-    return(
-        <div>
-            <h2>Counter - {state.count}</h2>
-            <button onClick={() => dispatch({ type: 'increment', payload: 10 })}>Increment 10</button>
-            <button onClick={() => dispatch({type: 'decrement', payload: 10})}>Decrement 10</button>
-        </div>
-    )
-}
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <div>
+      <h2>Counter - {state.count}</h2>
+      <button onClick={() => dispatch({ type: "decrement", payload: 10 })}>
+        Decrement 10
+      </button>
+      <button onClick={() => dispatch({ type: "increment", payload: 10 })}>
+        Increment 10
+      </button>
+      <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
+    </div>
+  );
+};
